61101182fb6877c49e267dd62b16fc1d38ca66ba
Glitch (glitch-hello-node) <none>
2024-11-29 13:18:20 +0000

    📉💁 Checkpoint ./package.json:72224620/1784 ./server.js:72224620/10415

--- server.js
+++ server.js
@@ -1,9 +1,45 @@
-const express = require("express");
-const app = express();
+const TelegramBot = require("node-telegram-bot-api");
 
-app.get("/", (req, res) => {
-  res.send("ربات تلگرام در حال اجرا است!");
+// توکن ربات خود را وارد کنید
+const token = "7300821157:AAFpqNZQqznNqf74O-gVDDhQHCdgzv4X8pY";
+
+// ایجاد ربات
+const bot = new TelegramBot(token, { polling: true });
+
+// آیدی عددی کانال مقصد و مبدا
+const sourceChannelId = "-1002421032578"; // کانال مبدا
+const destinationChannelId = "-1001337119090"; // کانال مقصد
+
+// آیدی عددی شما برای گزارش خطاها
+const userId = "7556830926";
+
+// زمانی که پیام جدیدی از شما دریافت می‌شود
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+
+  // بررسی اینکه آیا پیام از کاربر خودتان است
+  if (chatId === userId) {
+    // ارسال پیام به کانال مبدا
+    bot
+      .forwardMessage(sourceChannelId, chatId, msg.message_id)
+      .then(() => {
+        // در صورتی که پیام به کانال مبدا ارسال شد، به مقصد فوروارد می‌شود
+        bot
+          .forwardMessage(destinationChannelId, chatId, msg.message_id)
+          .catch((error) => {
+            // ارسال خطا به آیدی شما در صورت بروز خطا در ارسال پیام به کانال مقصد
+            bot.sendMessage(
+              userId,
+              `خطا در ارسال پیام به کانال مقصد: ${error.message}`
+            );
+          });
+      })
+      .catch((error) => {
+        // ارسال خطا به آیدی شما در صورت بروز خطا در ارسال پیام به کانال مبدا
+        bot.sendMessage(
+          userId,
+          `خطا در ارسال پیام به کانال مبدا: ${error.message}`
+        );
+      });
+  }
 });
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
